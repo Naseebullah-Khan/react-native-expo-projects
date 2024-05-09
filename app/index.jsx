41456ca008bf0ec1ba9c9533810098700e8cfe73
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2024-05-09 15:37:03 +0430

    Starter Project

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-base">Welcome to Aora!</Text>
+    </View>
+  );
+};
+
+export default App;
